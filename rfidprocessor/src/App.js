77097ae9016e43d7900993f1f675c4e16c1f7019
notjing <30694907+notjing@users.noreply.github.com>
2025-03-08 20:43:42 -0500

    made very basic website

---
+++ rfidprocessor/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> This is a website to manage RFID codes for MGCI Design Studio</p>
+      </header>
+
+      <div className="App-body">
+
+        <ul>
+          <li>RFID 1</li>
+          <li>RFID 2</li>
+          <li>RFID 3</li>
+        </ul>
+
+        <label id="">
+          <input name="test" type=""/>
+          <button type="submit"> Submit </button>
+        </label>
+      </div>
+      
+      
+
+    </div>
+  );
+}
+
+export default App;
